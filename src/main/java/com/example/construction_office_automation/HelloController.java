39506322f37924f59b938fd79fcb540d22db5fd5
JEDIBASIL <jedidiahbasil@gmail.com>
2022-08-26 21:05:08 +0100

    gave the side bar link fx id and the tabpane container, created a method in the controller to switch the selected pane

--- src/main/java/com/example/construction_office_automation/HelloController.java
+++ src/main/java/com/example/construction_office_automation/HelloController.java
@@ -2,13 +2,31 @@ package com.example.construction_office_automation;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.HBox;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+//   TAB LINKS
+    private HBox homeTabLink,projectsTabLink,workersTabLink,settingsTabLink;
+
+    @FXML
+//   APP TABPANE
+    private TabPane appTabPane;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+
+//   METHOD TO SWITCH APP TABPANE
+     public void switchPane(int tab){
+        appTabPane.getSelectionModel().select(tab);
+     }
+
+
 }
