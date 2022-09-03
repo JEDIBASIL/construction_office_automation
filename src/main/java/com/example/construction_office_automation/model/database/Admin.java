73c915cff9885cecb471a412cc61b35caea7c210
JEDIBASIL <jedidiahbasil@gmail.com>
2022-09-03 16:53:53 +0100

    generated getter and setter methods

--- src/main/java/com/example/construction_office_automation/model/database/Admin.java
+++ src/main/java/com/example/construction_office_automation/model/database/Admin.java
@@ -5,5 +5,27 @@ public class Admin {
     private String email;
     private String role;
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
