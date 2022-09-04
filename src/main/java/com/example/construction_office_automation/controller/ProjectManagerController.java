170a34f6343552993a3eef828f8f562058022857
JEDIBASIL <jedidiahbasil@gmail.com>
2022-09-04 20:37:22 +0100

    created a controller for the project manager scene

---
+++ src/main/java/com/example/construction_office_automation/controller/ProjectManagerController.java
@@ -0,0 +1,39 @@
+package com.example.construction_office_automation.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ProjectManagerController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+
+    protected void onAddWorkerClicked(){
+
+    }
+
+    @FXML
+
+    protected void onModalClose(){
+
+    }
+
+    @FXML
+
+    protected void  onAddDepartmentClicked(){
+
+    }
+
+    @FXML
+
+    protected void onAddProjectClicked(){
+
+    }
+}
