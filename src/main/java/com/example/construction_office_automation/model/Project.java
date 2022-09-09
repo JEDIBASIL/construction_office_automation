861a9f0a13540c7c168d7be831a776c534130c1b
JEDIBASIL <jedidiahbasil@gmail.com>
2022-09-09 23:33:26 +0100

    generated getter and setter method for the project model

--- src/main/java/com/example/construction_office_automation/model/Project.java
+++ src/main/java/com/example/construction_office_automation/model/Project.java
@@ -10,6 +10,87 @@ public class Project {
     private LocalDate startingDate;
     private LocalDate finishingDate;
     private String projectDescription;
-    private String projectStatus;
     private int projectProgress;
+    private String projectManager;
+    private String projectMonitor;
+
+    public long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(long projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectOwner() {
+        return projectOwner;
+    }
+
+    public void setProjectOwner(String projectOwner) {
+        this.projectOwner = projectOwner;
+    }
+
+    public String getProjectLocation() {
+        return projectLocation;
+    }
+
+    public void setProjectLocation(String projectLocation) {
+        this.projectLocation = projectLocation;
+    }
+
+    public LocalDate getStartingDate() {
+        return startingDate;
+    }
+
+    public void setStartingDate(LocalDate startingDate) {
+        this.startingDate = startingDate;
+    }
+
+    public LocalDate getFinishingDate() {
+        return finishingDate;
+    }
+
+    public void setFinishingDate(LocalDate finishingDate) {
+        this.finishingDate = finishingDate;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }
+
+    public int getProjectProgress() {
+        return projectProgress;
+    }
+
+    public void setProjectProgress(int projectProgress) {
+        this.projectProgress = projectProgress;
+    }
+
+    public String getProjectManager() {
+        return projectManager;
+    }
+
+    public void setProjectManager(String projectManager) {
+        this.projectManager = projectManager;
+    }
+
+    public String getProjectMonitor() {
+        return projectMonitor;
+    }
+
+    public void setProjectMonitor(String projectMonitor) {
+        this.projectMonitor = projectMonitor;
+    }
 }
